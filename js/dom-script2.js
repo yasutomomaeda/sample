51573654405e7277c86376441d29fff9e573c6d7
Yasutomo Maeda <pannwwpann@gmail.com>
2023-02-13 20:29:53 +0900

    最初の保存

---
+++ js/dom-script2.js
@@ -0,0 +1,9 @@
+button.addEventListener("mousedown",mouseDown);
+
+button.addEventListener("mouseup",mouseUp);
+
+button.addEventListener("click",buttonClick);
+
+form.addEventListener("submit",logSubmit);
+
+text.addEventListener("input",inputChange);
